90497be4fc27acd55551720d59e4c22e0bc5710a
Thirusha <thirusha1030@gmail.com>
2024-02-03 23:32:11 +0530

    error code

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const app=express();
 const port=8000; 
+app.use(
+    cors({
+      origin:  "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true
+    })
+  );
+app.use(express.json());
+app.options("/home", cors());
+
 app.get("/home",function(req,res)
 {
     res.send("<h1>Backend</h1>")
+})
+app.post("/save",function(req,res)
+{
+    var name=req.body.user
+    var age= req.body.pass
+    console.log(name , age)
+    res.json({msg:"received"})
 })
 app.listen(port, function(){
     console.log("server started on port 8000");
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import axios from 'axios';
+axios.defaults.withCredentials = true
+
 function Home(){
+    function send()
+    {
+       /* const name=document.getElementsByClassName('name')[0].value
+        const age=document.getElementsByClassName('age')[0].value
+            axios.post('http://localhost:8000/save',
+            {
+                user: name,
+                pass: age
+            }
+            ).then(function(response){
+                console.log(response)
+            })*/
+            axios.get('http://localhost:8000/home', {
+                withCredentials: true
+            })
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.error(error);
+            });
+    }
+
     return (
         <div>
             <h1>hey there!</h1>
+            <input type="text" className="name" placeholder="Name"></input>
+            <input type="number" className="age" placeholder="age"></input>
+            <button type="submit" onClick={send}></button>
         </div>
     )
 }
